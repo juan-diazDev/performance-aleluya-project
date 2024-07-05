f9ace7d9c8b854922d892af29f039afcfde8709a
unknown <juanpa.diaz108@gmail.com>
2024-07-05 02:29:56 -0500

    chore(web): addes many chore configuration

--- app/routes/_auth.signup.tsx
+++ app/routes/_auth.signup.tsx
@@ -1,4 +1,3 @@
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, Link } from '@remix-run/react';
 import {
   Button,
@@ -7,22 +6,19 @@ import {
   Label,
   TextField,
 } from 'react-aria-components';
-import { z } from 'zod';
 
-const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
-});
-
-type FormData = z.infer<typeof LoginSchema>;
-const resolver = zodResolver(LoginSchema);
-
-export default function LogIn() {
+export default function SignUp() {
   return (
     <main className="p-[2rem] flex items-center justify-center flex-col h-lvh bg-cyan-500">
-      <div className="flex justify-between flex-col w-[45%] items-center shadow-xl pt-24 pb-16 rounded-3xl bg-white">
-        <h1 className="text-3xl mb-12">Inicia Sesión</h1>
-        <Form className="flex flex-col gap-y-8 w-[75%]" method="POST">
+      <div className="flex justify-start flex-col w-[45%] items-center shadow-xl pt-24 pb-16 rounded-3xl bg-white">
+        <h1 className="text-3xl mb-8">Crea tu cuenta</h1>
+        <Form className="flex flex-col gap-y-3 w-[75%] max-h-[75%]">
+          <TextField className="flex flex-col gap-y-2" name="name" isRequired>
+            <Label className="text-lg">Nombre:</Label>
+            <Input className="border-2 border-cyan-500 rounded-xl px-4 py-3 hover:border-blue-500 focus:border-black" />
+            <FieldError className="text-red-600" />
+          </TextField>
+
           <TextField
             className="flex flex-col gap-y-2"
             name="email"
@@ -44,22 +40,21 @@ export default function LogIn() {
             <Input className="border-2 border-cyan-500 rounded-xl px-4 py-3 hover:border-blue-500 focus:border-black" />
             <span>
               <Link
-                to="/signup"
+                to="/login"
                 className="underline underline-offset-2 cursor-pointer hover:decoration-cyan-500 hover:text-cyan-500"
               >
-                ¿Aun no tienes una cuenta?
+                Ya tengo una cuenta
               </Link>
             </span>
-            <FieldError className="text-red-600" />
           </TextField>
-          <div className="mt-24 flex justify-center">
+          <span className="mt-10 flex justify-center">
             <Button
               onPress={() => console.log('You click me')}
               className="border-2 border-cyan-500 w-[90%] text-lg py-2 rounded-3xl text-white bg-cyan-500 hover:opacity-70"
             >
-              <b>Iniciar sesión</b>
+              <b>Crear cuenta</b>
             </Button>
-          </div>
+          </span>
         </Form>
       </div>
     </main>
---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  importOrder: ['^@remix/(.*)$', '^[~/]', '^/images/(.*)$', '^[../]', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
