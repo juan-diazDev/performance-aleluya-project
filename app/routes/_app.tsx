b2b13f470d56ba651d84e35a8f5e893a0ac7f03e
Juanpa <juanpa.diaz108@gmail.com>
2024-08-30 14:32:14 -0500

    refac(web): #0007 modifies non-user security access

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,4 +1,15 @@
+import { LoaderFunctionArgs } from '@remix-run/node';
+
+import { requireUser } from '~/utils/auth.service';
+
 import AppLayout from '~/componentes/app';
+
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const response = await requireUser(request);
+
+  return response;
+}
 
 export default function App() {
   return <AppLayout />;
